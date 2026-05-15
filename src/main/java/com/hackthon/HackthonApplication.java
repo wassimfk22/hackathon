2a73c3bba63fa5ca316d8e59a237ad537f19520e
Wassim <fkirwassoum22@gmail.com>
2026-05-16 00:20:58 +0100

    Initial commit

---
+++ src/main/java/com/hackthon/HackthonApplication.java
@@ -0,0 +1,13 @@
+package com.hackthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackthonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackthonApplication.class, args);
+	}
+
+}
